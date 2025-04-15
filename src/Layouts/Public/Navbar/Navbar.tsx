1761d6e7d307c6118bc5abc62ee31dd3ac3f77f8
Jasmeet-Chicmic <jasmeet.sidhu@chicmic.co.in>
2025-04-15 18:01:05 +0530

    fix: update navbar

--- src/Layouts/Public/Navbar/Navbar.tsx
+++ src/Layouts/Public/Navbar/Navbar.tsx
@@ -1,28 +1,28 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 
 import './navbar.scss';
+import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../../Shared/Constants';
 
 function NavbarComponent() {
+  const navigate = useNavigate();
   return (
     <Navbar expand="lg" className="">
       <Container fluid>
-        <Navbar.Brand href={ROUTES.HOMEPAGE}>BookeepAI</Navbar.Brand>
+        <Navbar.Brand href={ROUTES.LISTING}>BookeepAI</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto nav-links">
-            <Nav.Link href="#home" className="active">
-              Home
-            </Nav.Link>
-            <Nav.Link href="#link">About us</Nav.Link>
-            <Nav.Link href="#link">How it Works?</Nav.Link>
-          </Nav>
           <div className="sidebtns">
-            <Button className="bookademo ms-3" variant="primary">
-              Book a Demo
+            <Button
+              className="bookademo ms-3"
+              variant="primary"
+              onClick={() => {
+                navigate(ROUTES.HOMEPAGE);
+              }}
+            >
+              Upload Invoice
             </Button>
           </div>
         </Navbar.Collapse>
